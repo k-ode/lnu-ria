6b694e0d7bf773058de868fd5c909ee170bc958c
Kim Grönqvist <kimgronqvist@gmail.com>
2013-11-15 22:21:40 +0100

    Added a not so useful test to get running.

---
+++ test/spec-runner.js
@@ -0,0 +1,58 @@
+require.config({
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: [
+                'underscore',
+                'jquery'
+            ],
+            exports: 'Backbone'
+        },
+        backboneLocalStore: {
+            deps: ['backbone'],
+            exports: 'Store'
+        },
+        mocha: {
+            exports: 'mocha'
+        },
+        chai: {
+            exports: 'chai'
+        }
+    },
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        underscore: '../bower_components/underscore/underscore',
+        backbone: '../bower_components/backbone/backbone',
+        backboneLocalStorage: '../js/lib/backbone.localStorage.async/backbone.localStorage.async.js',
+        mocha: '../bower_components/mocha/mocha',
+        chai: '../bower_components/chai/chai'
+    }
+});
+
+window.store = 'TestStore'; // override Local storage store name for testing
+
+require([
+    'backbone',
+    'underscore',
+    'jquery',
+    'mocha',
+    'chai'
+], function (Backbone, _, $, mocha, chai) {    
+    // Chai
+    this.assert = chai.assert;
+    this.expect = chai.expect;
+
+    // Mocha
+    mocha.setup({ui: 'bdd', ignoreLeaks: true});
+
+    var specs = [];
+    specs.push('spec/namespace.spec');
+
+    require(specs, function() {
+        $(function() {
+            mocha.run();
+        });
+    });
+});
---
+++ test/spec/namespace.spec.js
@@ -0,0 +1,6 @@
+describe("Namespace", function () {
+    it("provides the 'Backbone' object", function () {
+        // Expect exists and is an object.
+        expect(Backbone).to.be.an("object");
+    });
+});
