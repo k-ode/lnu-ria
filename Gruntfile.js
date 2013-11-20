ef8fa8d9c93d263c398d4ae6365f291c83fe1417
Kim Grönqvist <kimgronqvist@gmail.com>
2013-11-20 02:56:07 +0100

    Missing requirejs from vendor path.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
                     { expand: true, cwd: 'bower_components/', src: ['jquery/jquery.js'], dest: 'vendor/' },
                     { expand: true, cwd: 'bower_components/', src: ['mocha/mocha.js'], dest: 'vendor/' },
                     { expand: true, cwd: 'bower_components/', src: ['mocha/mocha.css'], dest: 'vendor/' },
-                    { expand: true, cwd: 'bower_components/', src: ['require.js/require.js'], dest: 'vendor/' },
+                    { expand: true, cwd: 'bower_components/', src: ['requirejs/require.js'], dest: 'vendor/' },
                     { expand: true, cwd: 'bower_components/', src: ['underscore/underscore.js'], dest: 'vendor/' },
                     { expand: true, cwd: 'bower_components/', src: ['backbone/backbone.js'], dest: 'vendor/' }
                 ]
