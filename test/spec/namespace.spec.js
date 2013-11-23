9ef2e52cfedb9a278403bd1592491f1457908b37
Kim Grönqvist <kimgronqvist@gmail.com>
2013-11-23 03:03:17 +0100

    Remove not so useful test file

--- test/spec/namespace.spec.js
+++
@@ -1,6 +0,0 @@
-describe("Namespace", function () {
-    it("provides the 'Backbone' object", function () {
-        // Expect exists and is an object.
-        expect(Backbone).to.be.an("object");
-    });
-});
