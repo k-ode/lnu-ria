0f789317b765ce0ec0bb858e669e53c6b194f170
Kim Grönqvist <kimgronqvist@gmail.com>
2013-11-25 11:04:02 +0100

    Basic search and filter with CollectionBinder

--- js/router/router.js
+++ js/router/router.js
@@ -25,6 +25,7 @@ define(function (require) {
         RecipiesCollection = require('collection/recipies'),
 
         recipesCollection = new RecipiesCollection(recipies),
+        filteredCollection = new RecipiesCollection(recipies),
         homeView = new HomeView();
 
     var viewCreator = function (model) { return new HomeView({model: model}); };
@@ -33,7 +34,31 @@ define(function (require) {
 
     var collectionBinder = new Backbone.CollectionBinder(elManagerFactory);
 
-    collectionBinder.bind(recipesCollection, $('tbody'));
+    collectionBinder.bind(filteredCollection, $('tbody'));
+
+    var modelCreateCount = 3;
+    $('#createModel').on('click', function(){
+        modelCreateCount++;
+        filteredCollection.add({name: 'recept ' + modelCreateCount, category: 'yoyo', instructions: 'instruction four'});
+        recipesCollection.add({name: 'recept ' + modelCreateCount, category: 'yoyo', instructions: 'instruction four'});
+    });
+
+    $('#removeModel').on('click', function(){
+        if(filteredCollection.length > 0){
+            filteredCollection.remove(filteredCollection.at(filteredCollection.length - 1));
+            recipesCollection.remove(recipesCollection.at(recipesCollection.length - 1));
+        }
+    });
+
+    $('#resetCollection').on('click', function(){
+        filteredCollection.reset(recipesCollection.models);
+    });
+
+    $('#filter').on('keyup', function(e) {
+        var filterValue = $(e.currentTarget).val();
+        filteredCollection.reset(recipesCollection.models);
+        filteredCollection.reset(filteredCollection.filterCollection(filterValue));
+    });
 
     return Backbone.Router.extend({
         routes: {
